a62558b29d371d4be3a5e2016d71bf247740f24e
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-02-20 18:21:38 +0600

    recap practise done

--- recap.js
+++ recap.js
@@ -1,22 +1,26 @@
 // 1. let and const
-const hubby = 'Omor Sani';
-let phone = 'iphone 15';
-phone = 'Samsung Galaxy S17';
-// 2. default 
-// 5. spread or three dots (...)
-function maxNumber(array = []) {
-    const max = Math.max(...array);
-    return max;
-}
-const biggest = maxNumber();
-console.log(biggest);
+const fish = "Angel Fish";
+let myFish = "Albino Angel";
+myFish = "Oscar";
+// console.log(fish);
+// 2. default
+function fishPrice(fish1, fish2 = 200){
+    return fish1 + fish2;
+} 
+const allFishPrice = fishPrice(400, 500);
 // 3. template string
-const myNotes = `I am mojnu of ${hubby}. I have a ${phone}.`
-console.log(myNotes);
-
-// 4. arrow function
-// function square(x) {
-//     return x * x;
-// }
-const square = x => x * x;
-console.log(square(9));
+const myFishCollection = `I have a special couple fish, that is ${myFish} and its price is ${fishPrice(500)}. I love them so much.`;
+console.log(myFishCollection);
+// 4. Arrow function 
+/* function priceCalculate(fish1, fish2, fish3){
+    return fish1 + fish2 + fish3;
+}
+const totalPrice = priceCalculate(500, 300, 400);
+console.log(totalPrice); */
+const priceCalculate = (fish1, fish2, fish3) => fish1 + fish2 + fish3;
+const totalPrice = priceCalculate(500, 400, 300);
+console.log(totalPrice);
+// 5. spread or three dots (...)
+const fishList = ['albino angel', 'copper oscar', 'gourami', 'guppy', 'moly'];
+const secondFishList = [...fishList, 'butter', 'blue parrot'];
+console.log(secondFishList);
