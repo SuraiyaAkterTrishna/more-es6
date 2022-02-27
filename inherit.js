c45c4f57a70776a09870380a9c02f13d4543dbb0
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-02-27 16:09:46 +0600

    es6 topics

--- inherit.js
+++ inherit.js
@@ -1,23 +1,90 @@
-class TeamMember {
+class SupportTeam{
     name;
-    address = 'BD';
-    constructor(name, address) {
+    address = "Dhaka";
+    constructor(name, address){
         this.name = name;
-        this.address = address;
+        this.address =  address;
     }
 }
-class Support extends TeamMember {
+class Support extends SupportTeam{
     groupSupportTime;
-    designation = 'Support Web Dev';
-    constructor(name, address, time) {
-        super(name, address)
+    designation = "Web Developer";
+    constructor(name, address, time){
+        super(name, address);
         this.groupSupportTime = time;
     }
-    support(){
-        console.log(this.name,'Start a support Session');
-    }
 }
-const aamir = new Support('Aamir Khan', 'BD', 11);
-const salman = new Support('Solaiman Khan', 'Dubai', 4);
-aamir.support();
-console.log(aamir.name);
+const sagor = new Support('sagor', 'jashore', 7);
+console.log(sagor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class TeamMember {
+//     name;
+//     address = 'BD';
+//     constructor(name, address) {
+//         this.name = name;
+//         this.address = address;
+//     }
+// }
+// class Support extends TeamMember{
+//     groupSupportTime;
+//     designation = 'Super Web Develop';
+//     constructor(name, address, time){
+//         super(name, address)
+//         this.groupSupportTime = time;
+//     }
+// }
+// const alia = new Support('alia', 'mumbai', 4);
+// console.log(alia);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Support extends TeamMember {
+//     groupSupportTime;
+//     designation = 'Support Web Dev';
+//     constructor(name, address, time) {
+//         super(name, address)
+//         this.groupSupportTime = time;
+//     }
+//     support(){
+//         console.log(this.name,'Start a support Session');
+//     }
+// }
+// const aamir = new Support('Aamir Khan', 'BD', 11);
+// const salman = new Support('Solaiman Khan', 'Dubai', 4);
+// aamir.support();
+// console.log(aamir.name);
